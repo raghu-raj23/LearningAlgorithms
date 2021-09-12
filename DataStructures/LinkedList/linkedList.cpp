1a6c1c702c5c177dd92363f9399c9e8cd23b19d8
raghu-raj23 <raghuraj.muni@gmail.com>
2021-09-12 21:13:19 +0530

    singly lisked list applications

--- DataStructures/LinkedList/linkedList.cpp
+++ DataStructures/LinkedList/linkedList.cpp
@@ -37,7 +37,7 @@ void insertAtTail(Node* &head, int val){
 
 // Deletion
 
-void deleteAtHead(Node* &head, int val){
+void deleteAtHead(Node* &head){
     if(head == NULL){
         return;
     }
@@ -51,7 +51,7 @@ void deleteAtEnd(Node* &head, int val){
         return;
     }
     if(head->next == NULL){
-        deleteAtHead(head, val);
+        deleteAtHead(head);
         return;
     }
     Node* temp = head;
@@ -67,12 +67,11 @@ void deleteAtLoc(Node* &head, int val){
         return;
     }
     if(head->next == NULL){
-        deleteAtHead(head, val);
-    }
+        deleteAtHead(head);
+    } 
     Node* temp = head;
     while(temp->next->data != val){
         temp= temp->next;
-        cout<<temp->data<<" ";
     }
     Node* toDelete = temp->next;
     temp->next = temp->next->next;
@@ -87,7 +86,6 @@ Node* reverseIter(Node* &head){
     Node *prevPtr = NULL;
     Node *currPtr = head;
     Node *nextPtr;
-
     while(currPtr != NULL){
         nextPtr = currPtr->next;
         currPtr->next = prevPtr;
@@ -96,6 +94,75 @@ Node* reverseIter(Node* &head){
     }
     return prevPtr;
 }
+
+Node* reverseRec(Node* &head){
+    if(head == NULL || head->next == NULL) return head;
+    Node* newHead = reverseRec(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
+}
+
+Node* reverseK(Node* &head, int k){
+    Node* prevPtr = NULL;
+    Node* currPtr = head;
+    Node* nextPtr;
+    int count = 0;
+    while(currPtr != NULL && count <k){
+        nextPtr = currPtr->next;
+        currPtr->next = prevPtr;
+        prevPtr = currPtr;
+        currPtr = nextPtr;
+        count++;
+    }
+    if(nextPtr != NULL) head->next = reverseK(nextPtr, k);
+    return prevPtr;
+}
+
+void makecycle(Node* &head, int pos){
+    Node* temp = head;
+    Node* startNode;
+    int count = 1;
+    while(temp->next!= NULL){
+        if(count == pos){
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+
+//  Detecting loops in linked list
+
+bool detectLoop(Node* head){
+    Node* slowPtr = head;
+    Node* fastPtr = head;
+    while(fastPtr!= NULL and fastPtr->next != NULL){
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next->next;
+        if(fastPtr==slowPtr){
+            return true;
+        }
+    }
+    return false;
+}
+
+void removeLoop(Node* &head){
+    Node* slowPtr = head;
+    Node* fastPtr = head;
+    do{
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next->next;
+    }while(fastPtr != slowPtr);
+    fastPtr = head;
+    while(fastPtr->next != slowPtr->next){
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next;
+    }
+    slowPtr->next = NULL;
+}
+
 
 void display(Node* head){
     Node* temp = head;
@@ -126,12 +193,20 @@ int main(){
     insertAtHead(head,5);
     display(head);
     cout<<search(head, 7);
-    cout<<search(head, 3);
-    cout<<endl;
-    deleteAtLoc(head, 3);
+    cout<<search(head, 3)<<endl;
+    deleteAtHead(head);
     display(head);
-
     Node* newHead = reverseIter(head);
     display(newHead);
+    Node* newHeadRec = reverseRec(newHead);
+    display(newHeadRec);
+    int k = 2;
+    Node* newHeadK = reverseK(head,k);
+    display(newHeadK);
+    makecycle(head, 2);
+    cout<<detectLoop(head);
+    removeLoop(head);
+    cout<<detectLoop(head);
+    display(head);
     return 0;
 }
