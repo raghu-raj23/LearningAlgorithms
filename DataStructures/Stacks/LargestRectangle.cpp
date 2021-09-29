c1dfe7ad96712cb84dc2c5e7aeb601d7d817422e
raghu-raj23 <raghuraj.muni@gmail.com>
2021-09-29 22:43:57 +0530

    some leetcode problems

--- DataStructures/Stacks/LargestRectangle.cpp
+++ DataStructures/Stacks/LargestRectangle.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 int get_max_area(vector<int> a){
-    int n = a.size(), ans = 0, i = 0;
+    int n = a.size(), ans = 0, i = 0,curr = 0;
     stack<int> st;
-    a.push_back(0);
     while(i<n){
-        while(!st.empty() and a[st.top()]>a[i]){
+        while(!st.empty() and a[st.top()]>=a[i]){
             int t = st.top();
-            int h = a[t];
             st.pop();
-            if(st.empty()){
-                ans=max(ans, h*i);
-            }
-            else{
-                int len = i-st.top()-1;
-                ans = max(ans,h*len);
-            }
+            curr = a[t]*(st.empty() ? i : i-st.top()-1);
+            ans = max(ans,curr);
         }
         st.push(i);
         i++;
+    }
+    while(!st.empty()){
+        int t = st.top();
+        st.pop();
+        curr = a[t]*(st.empty() ? n :n-st.top()-1);
+        ans = max(ans, curr);
     }
     return ans;
 }
