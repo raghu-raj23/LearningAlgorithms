79c55529902588e641da21152202427e5eea5d66
raghu-raj23 <raghuraj.muni@gmail.com>
2021-10-09 23:35:50 +0530

    Binary tree problems

---
+++ DataStructures/Trees/Binary_Trees/heightBalancedBinaryTree.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int height(Node* node){
+    if(node == NULL){
+        return 0;
+    }
+    int lh =height(node->left);
+    int rh =height(node->right);
+    return max(lh,rh)+1;
+}
+
+bool isBalanced(Node* root){
+    if(root == NULL){
+        return true;
+    }
+    if(isBalanced(root->left) == false){
+        return false;
+    }
+    if(isBalanced(root->right) == false){
+        return false;
+    }
+    int lh = height(root->left);
+    int rh = height(root->right);
+    if(abs(lh - rh) <= 1){
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    if(isBalanced(root)){
+        cout<<"Balanced"<<endl;
+    }
+    else{
+        cout<<"Unbalanced"<<endl;
+    }
+
+    return 0;
+}
--- DataStructures/Trees/Binary_Trees/height_DiameterBinaryTree.cpp
+++ DataStructures/Trees/Binary_Trees/height_DiameterBinaryTree.cpp
@@ -23,7 +23,33 @@ int calcHeight(Node* root){
     return max(lheight, rheight)+1;
 }
 
-int calcDiameter(Node* root){}
+// longest path b/w any two leaves of a binary tree
+//  Time complexity: O(N^2)
+int calcDiameter(Node* root){
+    if(root == NULL) return 0;
+    int lHeight = calcHeight(root->left);
+    int rHeight = calcHeight(root->right);
+    int currDiameter = lHeight + rHeight + 1;
+    
+    int lDiameter = calcDiameter(root->left);
+    int rDiameter = calcDiameter(root->right);
+
+    return max(currDiameter, max(lDiameter, rDiameter));
+}
+
+int calcDiameterOptimised(Node* root, int* height){
+    if(root == NULL){
+        *height = 0;return 0;
+    }
+    int lHeight = 0, rHeight = 0;
+    int lDiameter = calcDiameterOptimised(root->left, &lHeight);
+    int rDiameter = calcDiameterOptimised(root->right, &rHeight);
+
+    int currDiameter = lHeight+rHeight +1;
+    *height = max(lHeight, rHeight) + 1;
+
+    return max(currDiameter, max(lDiameter, rDiameter));
+}
 
 int main()
 {
@@ -36,5 +62,8 @@ int main()
     root->right->right = new Node(7);
 
     cout<<"The height of the tree is: "<<calcHeight(root)<<endl;
+    cout<<"The diameter of the tree is: "<<calcDiameter(root)<<endl;
+    int height = 0;
+    cout<<"The optimised diameter of the tree is: "<<calcDiameterOptimised(root, &height)<<endl;
     return 0;
 }
