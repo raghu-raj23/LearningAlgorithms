8d9f3f20dd25c2716f652b0ef07d232e739dde61
raghu-raj23 <raghuraj.muni@gmail.com>
2021-10-13 23:14:11 +0530

    flatten binary tree and nodes at K

---
+++ DataStructures/Trees/Binary_Trees/flattenBinaryTree.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void flatten(Node* root){
+    if(root == NULL or (root->left == NULL or root->right == NULL)) return;
+    if(root-> left != NULL){
+        flatten(root-> left);
+
+        Node* temp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+
+        Node* t = root->right;
+        while(t->right != NULL){
+            t= t->right;
+        }
+        t->right = temp;
+    }
+    flatten(root->right);
+}
+
+// Time complexity: O(N)
+//  traverse from Right, Left, and Root.
+Node* previous = NULL;
+void flattenBetter(Node* root){
+    if(root == NULL) return;
+    flattenBetter(root->right);
+    flattenBetter(root->left);
+    root->right = previous;
+    root->left = NULL;
+    previous = root;
+}
+
+
+void inOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    inOrder(root);
+    cout<<endl;
+    flattenBetter(root);
+    inOrder(root);
+    cout<<endl;
+    return 0;
+}
---
+++ DataStructures/Trees/Binary_Trees/nodesAtK.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printSubTreeNodes(Node* root, int k){
+    if(root == NULL or k<0) return;
+    if(k==0){
+        cout<<root->data<<" ";return;
+    }
+    printSubTreeNodes(root->left, k-1);
+    printSubTreeNodes(root->right, k-1);
+}
+
+int printNodesAtK(Node* root, Node* target, int k){
+    if(root == NULL) return -1;
+    if(root == target){
+        printSubTreeNodes(root, k); return 0;
+    }
+    int dl = printNodesAtK(root->left, target, k);
+    if(dl!=-1){
+        if(dl+1 == k){
+            cout<<root->data<<" ";
+        }
+        else{
+            printSubTreeNodes(root->right, k-dl-2);
+        }
+        return dl+1;
+        
+        int dr = printNodesAtK(root->right, target, k);
+        if(dr+1 == k){
+            cout<<root->data<<" ";
+        }
+        else{
+            printSubTreeNodes(root->left, k-dr-2);
+        }
+        return dr+1;
+    }
+    return -1;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    printNodesAtK(root, root->left, 1);
+    cout<<endl;
+    return 0;
+}
